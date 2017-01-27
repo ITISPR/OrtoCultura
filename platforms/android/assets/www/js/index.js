363829a1f60995a4d3fb98cbf020b3a468f69c41
BATTIONI GIOVANNI (sBATTION3) <sBATTION3>
2017-01-27 12:10:57 +0100

    Implementata la pagina didattica con la chiamata ajax sugli ortaggi

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -19,51 +19,48 @@ var app = {
     }
 };
 
-
-
 function getCategories() {
-    'use strict';
+    'use strict'; //il codice JS deve essere eseguito in modalità strict 
     var url = "http://web.itis.pr.it:8080/ortocultura/wsSchedeOrtaggi/wsSchedeOrtaggi.php?callback=?";
     
     $.getJSON(url, 'service=getCategories', function (resp) {
         $("#didattica").empty();
-        var strCategories = '<br /><ul class="collapsible" data-collapsible="accordion">';
+        var strCategories = '<ul class="collapsible" data-collapsible="accordion">';
         
         $.each(resp, function (categoria, elements) {
-            var i, key;
-            
+            var i;
             strCategories += '<li><div class="collapsible-header"><img src="img/' + categoria + '.png" class="icon_categories"><span><b>' + categoria + '</b></span></div></div><div class="collapsible-body">';
             for (i = 0; i < elements.length; i += 1) {
-                strCategories += '<div data-id-ortaggio=' + elements[i].id + ' class="collapsible-header">' + elements[i].nome + '</div>';
+                strCategories += '<div onclick="getSingleVegetable()" data-id-ortaggio="' + elements[i].id + '" class="collapsible-header">' + elements[i].nome + '</div>';
             }
         });
-
-        
-        /*$(resp).each(function (categoria, element) {
-            //
-            window.alert(categoria);
-            $(resp[categoria]).each(function (ortaggio, el_o) {
-                window.alert(resp[categoria][ortaggio].nome);
-                //
-            });
-        });*/
-        
-                /*for (key in elements[i]) {
-                    if (elements[i].hasOwnProperty(key)) {
-                        console.log("Chiave: " + key + ", Valore: " + elements[i][key]);
-                    }
-                }*/
         
         strCategories += '</div></li>';
         $("#didattica").append(strCategories);
+        $('.collapsible').collapsible(); //initialize Materialize-css component
     });
+}
 
-    
+function getSingleVegetable() {
+    //'use strict';
+    //var url = "http://web.itis.pr.it:8080/ortocultura/wsSchedeOrtaggi/wsSchedeOrtaggi.php?callback=?";
+    //$.getJSON(url, 'service=getCategories', function (resp) {
+        $("#didattica").empty();
+        var strVegetable = '<h4 class="titoloOrtaggio">Pomodoro di Pachino</h4><br /><img class="circularImage" src="img/Pomodoro_Di_Pachino.png"><br /><br /><div class="container bloccoInformazioni"><b style="margin-right:20px;">Descrizione</b>Prodotto ortofrutticolo italiano a indicazione geografica protetta proveniente da parte delle provincie di Siracusa e Ragusa.<hr /><b style="margin-right:20px;">Luogo di Origine</b>Sicilia, Italia<hr /><b style="margin-right:20px;">Riconoscimento</b>I.G.P.</div>'
+        $("#didattica").append(strVegetable);
+        $('.collapsible').collapsible(); //initialize Materialize-css component
+    //});
 }
 
 $(document).ready(function () {
     'use strict';
     app.initialize();
+
+    var isCalled = false;
     
-    $("#id_li_didattica").click(getCategories);
+    $("#id_li_didattica").click(function () {
+        if (!isCalled) {
+            getCategories();
+        }
+    });
 });
--- www/js/index.js
+++ www/js/index.js
@@ -20,7 +20,7 @@ var app = {
 };
 
 function getCategories() {
-    'use strict';
+    'use strict'; //il codice JS deve essere eseguito in modalità strict 
     var url = "http://web.itis.pr.it:8080/ortocultura/wsSchedeOrtaggi/wsSchedeOrtaggi.php?callback=?";
     
     $.getJSON(url, 'service=getCategories', function (resp) {
@@ -31,14 +31,25 @@ function getCategories() {
             var i;
             strCategories += '<li><div class="collapsible-header"><img src="img/' + categoria + '.png" class="icon_categories"><span><b>' + categoria + '</b></span></div></div><div class="collapsible-body">';
             for (i = 0; i < elements.length; i += 1) {
-                strCategories += '<div data-id-ortaggio=' + elements[i].id + ' class="collapsible-header">' + elements[i].nome + '</div>';
+                strCategories += '<div onclick="getSingleVegetable()" data-id-ortaggio="' + elements[i].id + '" class="collapsible-header">' + elements[i].nome + '</div>';
             }
         });
         
         strCategories += '</div></li>';
         $("#didattica").append(strCategories);
-        $('.collapsible').collapsible();    //initialize Materialize-css component
+        $('.collapsible').collapsible(); //initialize Materialize-css component
     });
+}
+
+function getSingleVegetable() {
+    //'use strict';
+    //var url = "http://web.itis.pr.it:8080/ortocultura/wsSchedeOrtaggi/wsSchedeOrtaggi.php?callback=?";
+    //$.getJSON(url, 'service=getCategories', function (resp) {
+        $("#didattica").empty();
+        var strVegetable = '<h4 class="titoloOrtaggio">Pomodoro di Pachino</h4><br /><img class="circularImage" src="img/Pomodoro_Di_Pachino.png"><br /><br /><div class="container bloccoInformazioni"><b style="margin-right:20px;">Descrizione</b>Prodotto ortofrutticolo italiano a indicazione geografica protetta proveniente da parte delle provincie di Siracusa e Ragusa.<hr /><b style="margin-right:20px;">Luogo di Origine</b>Sicilia, Italia<hr /><b style="margin-right:20px;">Riconoscimento</b>I.G.P.</div>'
+        $("#didattica").append(strVegetable);
+        $('.collapsible').collapsible(); //initialize Materialize-css component
+    //});
 }
 
 $(document).ready(function () {
