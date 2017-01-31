f2c20ffc31b8a2236ac8cbe5cf53fe3069443c1b
Giovanni Battiopni <battionigiovanni@gm>
2017-01-31 18:06:02 +0100

    Aggiunta la possibilità di tornare indietro al menù principale dalla pagina login

--- www/js/index.js
+++ www/js/index.js
@@ -33,13 +33,16 @@ $(document).ready(function () {
 });
 
 
-function loginSchool() {
+function loginSchool(clickedImage) {
     //'use strict';
     //var url = "http://web.itis.pr.it:8080/ortocultura/wsSchedeOrtaggi/wsSchedeOrtaggi.php?callback=?";
     //$.getJSON(url, 'service=getCategories', function (resp) {
+        $("#menu").empty();
         $("#orti").empty();
-        var strLogin = '<div style="padding: 30px;"><h4 class="titoloLogin">Accedi al tuo orto</h4><br><br><div class="input-field col s6"><input id="inInput" type="text"><label id="lblInput" for="username">Username</label></div><br /><div class="input-field col s6"><input id="inInput" type="password"><label id="lblInput" for="last_name">Password</label></div><br><div class="center"><a class="waves-effect waves-light btn btnLogin">login</a><br><br><a href="" style="color: #008744;"><u>registrati</u></a></div></div>'
-        $("#orti").append(strLogin);
+        var strMenuLogin = '<nav class="nav-extended"><div class="nav-wrapper"><a href="#" class="brand-logo"><img src="img/logoWhite.png" width="56" height="56"></a><a href="" class="button-collapse"><img src="img/ic_arrow_back_white.png" style="margin-top: 45%;"></a><ul class="tabs tabs-transparent"><li class="tab"><a class="active" href="#orti"><b>Login</b></a></li></ul></div></nav>'
+        var strLoginOrto = '<div style="padding: 30px;"><br><br><div class="input-field col s6"><input id="inInput" type="text"><label id="lblInput" for="username">Username</label></div><br /><div class="input-field col s6"><input id="inInput" type="password"><label id="lblInput" for="last_name">Password</label></div><br><div class="center"><a class="waves-effect waves-light btn btnLogin">login</a><br><br><a href="" style="color: #008744;"><u>registrati</u></a></div></div>'
+        $("#menu").append(strMenuLogin);
+        $("#orti").append(strLoginOrto);
         $('.collapsible').collapsible(); //initialize Materialize-css component
     //});
 }
