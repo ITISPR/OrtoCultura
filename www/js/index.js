4d31dd64ac949d80b778ad3430dd3dc105165ab9
BATTIONI GIOVANNI (sBATTION3) <sBATTION3>
2017-01-31 12:11:06 +0100

    Sposta funziona js da index.html a index.js

--- www/js/index.js
+++ www/js/index.js
@@ -31,3 +31,15 @@ $(document).ready(function () {
     'use strict';
     app.initialize();
 });
+
+
+function loginSchool() {
+    //'use strict';
+    //var url = "http://web.itis.pr.it:8080/ortocultura/wsSchedeOrtaggi/wsSchedeOrtaggi.php?callback=?";
+    //$.getJSON(url, 'service=getCategories', function (resp) {
+        $("#orti").empty();
+        var strLogin = '<div style="padding: 30px;"><h4 class="titoloLogin">Accedi al tuo orto</h4><br><br><div class="input-field col s6"><input id="inInput" type="text"><label id="lblInput" for="username">Username</label></div><br /><div class="input-field col s6"><input id="inInput" type="password"><label id="lblInput" for="last_name">Password</label></div><br><div class="center"><a class="waves-effect waves-light btn btnLogin">login</a><br><br><a href="" style="color: #008744;"><u>registrati</u></a></div></div>'
+        $("#orti").append(strLogin);
+        $('.collapsible').collapsible(); //initialize Materialize-css component
+    //});
+}
