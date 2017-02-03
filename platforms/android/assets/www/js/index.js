d4e32ca63b6ed9c794d7363be2810fe92bb6ead9
BATTIONI GIOVANNI (sBATTION3) <sBATTION3>
2017-02-03 11:19:44 +0100

    Piccole modifiche eseguite sia nella pagina orti che in quella di login

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -31,3 +31,18 @@ $(document).ready(function () {
     'use strict';
     app.initialize();
 });
+
+
+function loginSchool() {
+    //'use strict';
+    //var url = "http://web.itis.pr.it:8080/ortocultura/wsSchedeOrtaggi/wsSchedeOrtaggi.php?callback=?";
+    //$.getJSON(url, 'service=getCategories', function (resp) {
+        $("#menu").empty();
+        $("#orti").empty();
+        var strMenuLogin = '<nav class="nav-extended"><div class="nav-wrapper"><a href="#" class="brand-logo"><img src="img/logoWhite.png" width="56" height="56"></a><a href="" class="button-collapse"><img src="img/ic_arrow_back_white.png" style="margin-top: 45%;"></a><ul class="tabs tabs-transparent"><li class="tab"><a class="active" href="#orti"><b>Login</b></a></li></ul></div></nav>'
+        var strLoginOrto = '<div style="padding: 30px;"><br><br><div class="input-field col s6"><input id="inInput" type="text"><label id="lblInput" for="username">Username</label></div><br /><div class="input-field col s6"><input id="inInput" type="password"><label id="lblInput" for="last_name">Password</label></div><br><div class="center"><a class="waves-effect waves-light btn btnLogin">login</a><br><br><a class="waves-effect waves-light btnGuest btn" style="color: #008744; letter-spacing: 0px;">Ospite</a></div></div>'
+        $("#menu").append(strMenuLogin);
+        $("#orti").append(strLoginOrto);
+        $('.collapsible').collapsible(); //initialize Materialize-css component
+    //});
+}
