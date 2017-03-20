e22c8e2dd51c7085833885047dcd294898dd4052
BATTIONI GIOVANNI (sBATTION3) <sBATTION3>
2017-03-20 09:14:01 +0100

    Merge a mano da 'didattica' a 'demo'

--- www/js/index.js
+++ www/js/index.js
@@ -26,13 +26,6 @@ var app = {
     }
 };
 /****************************/
-
-$(document).ready(function () {
-    'use strict';
-    app.initialize();
-});
-
-
 function loginSchool() {
     //'use strict';
     //var url = "http://web.itis.pr.it:8080/ortocultura/wsSchedeOrtaggi/wsSchedeOrtaggi.php?callback=?";
@@ -46,3 +39,91 @@ function loginSchool() {
         $('.collapsible').collapsible(); //initialize Materialize-css component
     //});
 }
+/****************************/
+function getFamilies() {
+    'use strict'; //il codice JS deve essere eseguito in modalità strict 
+    var url = "http://web.itis.pr.it:8080/ortocultura/wsSchedeOrtaggi/wsSchedeOrtaggi.php?callback=?";
+    $.getJSON(url, 'service=getFamilies', function (resp) {
+        $("#didattica").empty();
+        var strCategories = '<ul class="collapsible" data-collapsible="accordion">';
+        
+        $.each(resp, function (keyFamiglia, famiglia) {
+            var i;
+            strCategories += '<li><div onclick="getCategories(this)" class="collapsible-header families"><span>' + famiglia.nome + '</span></div><div class="collapsible-body"></div></li>';
+        });
+        
+        $("#didattica").append(strCategories);
+        $('.collapsible').collapsible();
+    });
+}
+
+function getCategories(divClicked) {
+    'use strict';
+    var url = "http://web.itis.pr.it:8080/ortocultura/wsSchedeOrtaggi/wsSchedeOrtaggi.php?callback=?";
+    
+    $.getJSON(url, 'service=getCategories&famiglia=' + $(divClicked).parent().find(".collapsible-header span").html(), function (resp) {
+        var strCategories = '<ul class="collapsible" data-collapsible="accordion">';
+
+        $.each(resp, function (keyCategoria, categoria) {
+            var i;
+            strCategories += '<li><div class="collapsible-header categories"><img src="' + categoria.img 'class="icon_categories/><span>' + categoria.nome + '</span></div><div class="collapsible-body"></div</li>';
+        }); 
+
+        $(divClicked).parent().find(".collapsible-body").html(strCategories);
+        $('.collapsible').collapsible();
+    });
+    
+    
+}
+
+/*function getCategories(famiglia) {
+    'use strict';
+    var url = "http://web.itis.pr.it:8080/ortocultura/wsSchedeOrtaggi/wsSchedeOrtaggi.php?callback=?";
+    
+    $.getJSON(url, 'service=getCategories', function (resp) {
+        var strCategories = '<ul class="collapsible" data-collapsible="accordion">';
+        
+        $.each(resp, function (categoria, elements) {
+            var i;
+            strCategories += '<li><div class="collapsible-header"><img src="img/' + categoria + '.png" class="icon_categories"><span><b>' + categoria + '</b></span></div></div><div class="collapsible-body">';
+            for (i = 0; i < elements.length; i += 1) {
+                strCategories += '<div onclick="getSingleVegetable()" data-id-ortaggio="' + elements[i].id + '" class="collapsible-header">' + elements[i].nome + '</div>';
+            }
+        });
+        
+        strCategories += '</div></li>';
+        $("#didattica").append(strCategories);
+        $('.collapsible').collapsible(); //initialize Materialize-css component
+    });
+}*/
+
+/*
+function getSingleVegetable() {
+        'use strict';
+        //var url = "http://web.itis.pr.it:8080/ortocultura/wsSchedeOrtaggi/wsSchedeOrtaggi.php?callback=?";
+        //$.getJSON(url, 'service=getCategories', function (resp) {
+        $("#menu").empty();
+        $("#didattica").empty();
+        var strMenu = '<nav class="nav-extended"><div class="nav-wrapper" style="height: 65px;"><a href="#" class="brand-logo"><img src="img/logoWhite.png" width="56" height="56"></a><a href="" class="button-collapse"><img src="img/ic_arrow_back_white.png" style="margin-top: 45%;"></a><ul class="tabs tabs-transparent"><li class="tab"><a class="active" href="#orti"><b></b></a></li></ul></div></nav>'
+        var strVegetable = '<h4 class="titoloOrtaggio">Pomodoro di Pachino</h4><br /><img class="circularImage" src="img/Pomodoro_Di_Pachino.png"><br /><br /><div class="container bloccoInformazioni"><b style="margin-right:20px;">Descrizione</b>Prodotto ortofrutticolo italiano a indicazione geografica protetta proveniente da parte delle provincie di Siracusa e Ragusa.<hr /><b style="margin-right:20px;">Luogo di Origine</b>Sicilia, Italia<hr /><b style="margin-right:20px;">Riconoscimento</b>I.G.P.</div>'
+        $("#menu").append(strMenu);
+        $("#didattica").append(strVegetable);
+        $('.collapsible').collapsible(); //initialize Materialize-css component
+    //});
+}
+*/
+/****************************/
+
+$(document).ready(function () {
+    'use strict';
+    app.initialize();
+    
+    var isCalled = false;
+    
+    $("#id_li_didattica").click(function () {
+        if (!isCalled) {
+            getFamilies();
+        }
+    });
+});
+
